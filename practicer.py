cbaf9127e36727ad596fb1241e6b30039db79c65
djpalumbo <davejpalumbo@gmail.com>
2017-05-04 19:56:09 -0400

    Borrowed/repurposed some tkinter stopwatch code, added some functionality

--- practicer.py
+++ practicer.py
@@ -8,97 +8,90 @@
 # https://www.tutorialspoint.com/python/python_gui_programming.htm
 # http://stackoverflow.com/questions/27112614/tkinter-gui-stopwatch-timer
 #
+# Timer:
+# http://code.activestate.com/recipes/124894-stopwatch-in-tkinter/
+#
 ################################################################################
+from tkinter import *
 import time
 import os
 import random
-import tkinter
 
 random.seed()
-startTime = 0
-prevTime = 0
-currentTime = 0
-timing = False
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 
 
+# Timer menu needs:
+#   Next
+#   Done
+#   Log of submissions (incl. each timestamp; will be available at the end)
+
 
 def getNextProb():
   global dir_path
   os.chdir(dir_path + "\\problems")
-
   for traversals in range(0,2):
     subdirs = os.listdir()
     randNum = random.randint(0, len(subdirs) - 1)
     os.chdir(os.getcwd() + "\\" + subdirs[randNum])
-    if DEBUG:
-      print(os.getcwd())
-
   os.startfile(os.getcwd() + "\\" + subdirs[randNum][3:] + ".pdf")
 
 
-# TIMER MENU
-# Should include the following buttons:
-#   Start
-#   Next Problem
-#   Done
-# As well as:
-#   Log of submissions (incl. each timestamp; will be available at the end)
-#   The actual timer
+class StopWatch(Frame):
+  """ Implements a stop watch frame widget. """
+  def __init__(self, parent=None, **kw):
+    Frame.__init__(self, parent, kw)
+    self._start = 0.0
+    self._elapsedtime = 0.0
+    self._running = 0
+    self.timestr = StringVar()
+    self.makeWidgets()
 
-def getTimePassed():
-  global currentTime
-  global startTime
-  print(str(int((currentTime - startTime) % 86400 / 3660)) + ":" +
-        str(int((currentTime - startTime) % 3600 / 60)) + ":" +
-        str(int((currentTime - startTime) % 60)))
+  def makeWidgets(self):
+    """ Make the time label. """
+    l = Label(self, textvariable=self.timestr)
+    self._setTime(self._elapsedtime)
+    l.pack(fill=X, expand=NO, pady=2, padx=2)
 
+  def _update(self):
+    """ Update the label with elapsed time. """
+    self._elapsedtime = time.time() - self._start
+    self._setTime(self._elapsedtime)
+    self._timer = self.after(50, self._update)
 
-def startTimer():
-  global startTime, prevTime, currentTime
+  def _setTime(self, elap):
+    """ Set the time string to Minutes:Seconds:Hundreths """
+    minutes = int(elap/60)
+    seconds = int(elap - minutes*60.0)
+    hseconds = int((elap - minutes*60.0 - seconds)*100)
+    self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
 
-  startTime = prevTime = currentTime = time.time()
-  timing = True
-  print("timing = " + str(timing))
+  def Start(self):
+    """ Start the stopwatch & give first problem, ignore if running. """
+    if not self._running:
+      self._start = time.time() - self._elapsedtime
+      self._update()
+      self._running = 1
+      getNextProb()
 
-
-def updateTimer():
-  global currentTime, prevTime, timing
-
-  while timing:
-    time.sleep(0.1)
-    currentTime = time.time()
-    if (currentTime - prevTime) >= 1:
-      prevTime = currentTime
-      getTimePassed()
-
-
-def stopTimer():
-  timing = False
-  print("timing = " + str(timing))
-
-
-def startGUI():
-  global timing
-  gui = tkinter.Tk()
-
-  start = tkinter.Button(gui, text = "Start", command = startTimer())
-  start.pack()
-  stop = tkinter.Button(gui, text = "Stop", command = stopTimer())
-  stop.pack()
-
-  while True:
-    gui.update_idletasks()
-    gui.update()
-    if timing:
-      start.destroy()
-      print("destroyed")
+  def Next(self):
+    """ Go to next problem if running. """
+    if self._running:
+      getNextProb()
 
 
 def main():
-  startGUI()
+  root = Tk()
+  sw = StopWatch(root)
+  sw.pack(side=TOP)
 
+  Button(root, text='Start', command=sw.Start).pack(side=LEFT)
+  Button(root, text='Next', command=sw.Next).pack(side=LEFT)
+  Button(root, text='Quit', command=root.quit).pack(side=RIGHT)
 
-main()
+  root.mainloop()
+
+if __name__ == '__main__':
+  main()
